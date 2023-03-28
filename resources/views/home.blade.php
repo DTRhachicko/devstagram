025b4809da4b72cb238dcb3a3b013023645cad1c
Jonathan Limon MacMini <jonathan_lim20@outlook.com>
2023-03-27 18:00:04 -0700

    Se creo un componente para listar post en home y dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,29 +6,5 @@
 
 
 @section('contenido')
-    @if ($posts->count())
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user ]) }}">
-                        <img src="{{ asset('uploads') . "/" . $post->imagen }}" alt="imagen del post {{ $post->titulo }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-
-        <!-- Pagination -->
-        <div class="mt-10">
-            {{ $posts->links('pagination::tailwind') }}
-        </div>
-    @else
-        <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
-    @endif
-
-    <!-- Opción 2 de mostrar cosas con condicional
-    @forelse ($posts as $post)
-        <h1>{{ $post->titulo }}</h1>
-    @empty
-        <p>No hay posts</p>
-    @endforelse-->
+    <x-listar-post :posts="$posts" />
 @endsection
